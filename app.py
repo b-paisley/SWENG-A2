37d1848de12bdde554bb34182fb9188226f193f9
sultan <sultanalbo06@gmail.com>
2022-11-07 16:42:49 +0000

    working front end

--- app.py
+++ app.py
@@ -2,20 +2,31 @@ from flask import Flask, request, render_template
 import calc
 
 app = Flask(__name__)
-print(__name__)
+
+def calc_exe(input_str: str):
+    """
+    It takes a string, converts it to a list, and then calls the calc function on that list
+
+    :param input_str: The string to be converted to a list and then calculated
+    :type input_str: str
+    :return: The result of the calculation.
+    """
+    input_list = calc.to_list(input_str)
+    return round(calc.calc(input_list), 3)
+
+
 @app.route('/', methods=['POST', 'GET'])
 def calculate():
     result = " "
     error = " "
     if request.method == 'POST':
         try:
-            result = calc.calc_exe(input_str=request.form.get('user_input'))
+            result = calc_exe(input_str=request.form.get('user_input'))
         except Exception as err:
             error = err
 
-    print(result)
     return render_template('tamplate.html', RESULT=result, ERROR=error)
 
+
 if __name__ == "__main__":
-    print("running")
     app.run(host='localhost', port=5000)
